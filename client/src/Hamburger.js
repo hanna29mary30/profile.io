de8672249864761277d8bef18247671c9106a30c
Hanna <hanna29mary30@gmail.com>
2023-09-28 08:23:27 +0530

    profile

--- client/src/Hamburger.js
+++ client/src/Hamburger.js
@@ -2,19 +2,17 @@ import React from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import { NavLink } from "react-router-dom";
+
 export const Hamburger = () => {
   return (<div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <ul class="navbar-nav mr-auto">
-			
-			<li class="nav-item dropdown">
-				<a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> <span class="navbar-toggler-icon"></span> </a>
-			    <ul class="dropdown-menu dropdown-menu-end">
+        <ul class="navbar-nav ml-auto">
+			<a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-placement="bottom" data-bs-offset="10,10"> <span class="navbar-toggler-icon "></span> </a>
+			    <ul class="dropdown-menu dropdown-menu">
 				  <li><a class="dropdown-item" href="/"> Profile</a></li>
 				  <li><a class="dropdown-item" href="/todo"> Todo app</a></li>
 			    </ul>
-			</li>
+			
 		</ul>
 	
  
--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -1,6 +1,7 @@
+import "./Todo.css";
 import { useState, useEffect } from "react";
 import { addNewTodo, getTodos} from "../api";
-import "./Todo.css";
+
 const Todo = () => {
   const val = { data: "", completed: false };
   const [text, setTodo] = useState([val]);
@@ -45,42 +46,43 @@ const Todo = () => {
           />
         </form>
       </div>
-      <div>
-        <table className="table table-bordered table-hover table-striped text-center">
-          <thead className="table-dark">
-            <tr>
-              <th style={{ height: "50px" }}>Todo</th>
-              <th style={{ height: "50px" }} colSpan={2}>
-                Status
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {text.map((user) => (
-              <tr
-                key={user._id}
-                style={{
-                  
-                  textDecoration: user.completed ? "line-through" : "none",
-                }}
-              >
-                <td>{user.data}</td>
-                <td>
-                  <div className="d-flex justify-content-center">
-                    <button
-                      className="btn btn-success m-2"
-                      onClick={() => markasdone(user._id)}
-                    >
-                      Mark as Done
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="row justify-content-center">
+    <table className="table table-bordered table-hover table-striped text-center" style={{ width: '80%', margin: '0 auto' }}>
+      <thead className="table-dark">
+        <tr>
+          <th style={{ height: "50px" }}>Todo</th>
+          <th style={{ height: "50px" }} colSpan={2}>
+            Status
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {text.map((user) => (
+          <tr
+            key={user._id}
+            style={{
+              textDecoration: user.completed ? "line-through" : "none",
+            }}
+          >
+            <td>{user.data}</td>
+            <td>
+              <div className="d-flex justify-content-center">
+                <button
+                  className="btn btn-success m-2"
+                  onClick={() => markasdone(user._id)}
+                >
+                  Mark as Done
+                </button>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+      
       </div>
-    </div>
+   
   );
 };
 export default Todo;
